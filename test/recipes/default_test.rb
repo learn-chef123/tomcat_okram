c6397f4cb576e54f260c3b7c84eb8e99c4d0e7b1
okram999 <nirish.okram@gmail.com>
2016-12-11 09:32:30 -0600

    Adds integration test cases for kitchen verify

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,14 +5,31 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+# unless os.windows?
+#   describe user('root') do
+#     it { should exist }
+#     skip 'This is an example test, replace with your own test.'
+#   end
+# end
+#
+# describe port(80) do
+#   it { should_not be_listening }
+#   skip 'This is an example test, replace with your own test.'
+# end
+
+describe group('tomcat') do
+  it { should exist }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe user('tomcat') do
+  it { should exist}
+end
+
+describe file('/opt/tomcat') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file('/etc/systemd/system/tomcat.service') do
+  it { should exist }
 end
